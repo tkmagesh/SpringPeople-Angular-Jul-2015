45cdbfcb166cfe7183e542d43baea53c29e43031
tkmagesh <tkmagesh77@gmail.com>
2015-08-01 11:42:36 +0530

    Async - Using Events

--- Day-02/06-Async/async.js
+++ Day-02/06-Async/async.js
@@ -12,7 +12,7 @@ function addClient(x,y){
     console.log("[SC] result = ", result);
 }
 
-/* Async */
+/* Async using callback*/
 function addAsync(x,y,onResult){
     console.log("[SP] received the values");
     setTimeout(function(){
@@ -29,3 +29,59 @@ function addAsyncClient(x,y){
         console.log("[SC] result = ", result);
     });
 }
+
+/* Async using events */
+function getAdder(){
+    var _callbacks = [];
+    return {
+        add : function(x,y){
+            console.log("[SP] received the values");
+            setTimeout(function(){
+                var result = x + y;
+                console.log("[SP] returning the result");
+                _callbacks.forEach(function(callback){
+                    callback(result);
+                })
+            },3000);
+        },
+        addCallback : function(callback){
+            _callbacks.push(callback);
+        }
+    }
+}
+
+
+var adder = getAdder();
+adder.addCallback(function(result){
+    console.log("[SC] result = ", result);
+});
+adder.add(100,200);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
